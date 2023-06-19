cb3d4f63688e53210088eb4429f69b2274f4a4d3
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-06-19 10:45:09 +0500

    showpassword

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,33 @@ class MyField extends StatefulWidget {
 }
 
 class _MyFieldState extends State<MyField> {
+  bool _showPassword = false;
+  Icon icon = Icon(Icons.visibility);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         children: [
           TextFormField(
-            decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
-            inputFormatters: [FilteringTextInputFormatter.allow(RegExp('[0-9 ,a-z]'))],
+            obscureText: !this._showPassword,
+            decoration: InputDecoration(
+                prefixIcon: Icon(Icons.security),
+                suffixIcon: IconButton(
+                    color: _showPassword ? Colors.blue : Colors.grey,
+                    onPressed: () {
+                      _showPassword
+                          ? icon = Icon(Icons.visibility)
+                          : icon = Icon(Icons.visibility_off);
+                      setState(() {
+                        this._showPassword = !this._showPassword;
+                      });
+                    },
+                    icon: icon),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10))),
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp('[0-9 ,a-z]'))
+            ],
           )
         ],
       ),
